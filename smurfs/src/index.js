2a5ee27cbe6faeeb85ab98e05d7f65c28162a71d
Annette Odingo <odingoannette@gmail.com>
2019-12-13 08:46:04 +0000

    Add thunk as a middleware and plug store to the application

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {combineReducers, createStore,compose, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const monsterReducer = combineReducers({
+    data: reducer,
+})
+const store = createStore(monsterReducer, {},
+    compose(
+      applyMiddleware(thunk /* ,etc , other middlewares */),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+    );
+
+ReactDOM.render(
+<Provider>
+<App />
+</Provider>,
+ document.getElementById("root"));
